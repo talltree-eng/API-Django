664967fb7c81a7b6b94cd800db954f06fe4f376d
talltree-eng <talltree.eng000@outlook.com>
2023-07-16 23:51:52 +0000

    fix

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'api-django-5.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'api-django-5-e93439fb77b5.herokuapp.com']
 
 
 # Application definition
