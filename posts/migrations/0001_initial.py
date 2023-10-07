b2b7380817d58b6f914336d5d5d3503ab09c084a
talltree-eng <talltree.eng000@outlook.com>
2023-10-07 19:02:10 +0000

    Some edits

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('title', models.CharField(max_length=255)),
                 ('content', models.TextField(blank=True)),
-                ('image', models.ImageField(blank=True, default='../default_post_hde9ti', upload_to='images/')),
+                ('image', models.ImageField(blank=True, default='../default_post_rn4qcl', upload_to='images/')),
                 ('owner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('name', models.CharField(blank=True, max_length=255)),
                 ('content', models.TextField(blank=True)),
-                ('image', models.ImageField(default='../default_profile_hcq3po', upload_to='images/')),
+                ('image', models.ImageField(default='../default_profile_ftyuqx', upload_to='images/')),
                 ('owner', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
