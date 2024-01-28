14079655123e06aca1eb2563832c294136282ea9
talltree-eng <talltree.eng000@outlook.com>
2024-01-28 17:24:48 +0000

    Update model construction

--- likes/models.py
+++ likes/models.py
@@ -4,10 +4,8 @@ from posts.models import Post
 
 
 class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
     account_owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(
-        Post, related_name='likes', on_delete=models.CASCADE
-    )
     made_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
