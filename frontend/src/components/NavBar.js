99c6b96578ebc573f237da1adb212fa723331c20
talltree-eng <talltree.eng000@outlook.com>
2024-01-28 20:42:57 +0000

    Remove import for Navbar Brand logo

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
