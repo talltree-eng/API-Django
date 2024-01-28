782d9ebb77ec977744038f1906055ff38f8c4145
talltree-eng <talltree.eng000@outlook.com>
2024-01-28 17:26:08 +0000

    Update model construction

--- followers/models.py
+++ followers/models.py
@@ -3,12 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Follower(models.Model):
-    account_owner = models.ForeignKey(
-        User, related_name='following', on_delete=models.CASCADE
-    )
-    followed = models.ForeignKey(
-        User, related_name='followed', on_delete=models.CASCADE
-    )
+    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
+    account_owner = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     made_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
